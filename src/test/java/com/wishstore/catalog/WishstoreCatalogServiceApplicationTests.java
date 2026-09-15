405df6e871f4b5079a5629d8bc7faf8178e7d94d
Juanes_URBA <024juanurrego@gmail.com>
2026-09-15 02:32:04 -0500

    feat: create repository

---
+++ src/test/java/com/wishstore/catalog/WishstoreCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishstore.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishstoreCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
